94290f04d306c04f400dc9615ac6ec3f22449b11
bel-lov <belou.lyuba@yandex.ru>
2022-02-28 19:53:16 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.main-slider', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: true,
+   // Navigation arrows
+  navigation: {
+    nextEl: '.main-slider-arrow',
+  },
+  });
